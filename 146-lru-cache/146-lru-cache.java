3202781bcdf06e6de8715893bf57100c8643c287
Atul Aditya <atul100aditya@gmail.com>
2022-01-04 22:15:43 +0530

    Time: 100 ms (15.12%), Space: 130.8 MB (23.21%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,91 +1,82 @@
-class Node {
-    Node prev;
-    Node next;
+class Node{
+    int value;
     int key;
-    int val;
-    Node(int key, int val) {
-        this.val = val;
-        this.next = null;
-        this.prev = null;
-        this.key = key;
+    Node next;
+    Node prev;
+    Node(int v,int k){
+        value = v;
+        key = k;
+        next = null;
+        prev = null;
     }
 }
 class LRUCache {
-    Map<Integer, Node> cache;
+    Map<Integer, Node> LRUCacheMap;
+    int capacity;
+    int count;
     Node head;
     Node tail;
-    int capacity;
     public LRUCache(int capacity) {
-        this.cache = new HashMap<>();
-        this.head = new Node(0, 0);
-        this.tail = new Node(0, 0);
+        LRUCacheMap = new HashMap<>();
         this.capacity = capacity;
-        setHeadTail();
+        this.count = 0;
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        intializeNode(head,tail);
     }
-    
-    public int get(int key) {
-        Node node = this.cache.getOrDefault(key, null);
-        if (node == null ) {
-            return -1;
-        }
-        detachNode(node);
-        updateNodeToHead(node);
-        return node.val;
-    }
-    
-    public void put(int key, int value) {        
-        Node node = this.cache.getOrDefault(key, null);
-        if (node == null && capacity == this.cache.size()) {
-            int keyToEvit = evitFromTail();
-            this.cache.remove(keyToEvit);
-        }
-        if (node == null){
-            node = new Node(key, value);
-            this.cache.put(key, node);
-        }else {
-            node.val = value;
-            detachNode(node);
-        }
-        updateNodeToHead(node);
-    }
-      
-    void setHeadTail() {
-        this.head.next = this.tail;
-        this.tail.prev = this.head;
-    }
-    
-    int evitFromTail() {
-        Node temp = tail.prev;
-        Node tempPrev = temp.prev;
-        tempPrev.next = tail;
-        tail.prev = tempPrev;
-        return temp.key;
-    }
-    
-    void detachNode(Node node) {
-        Node next = node.next;
-        Node prev = node.prev;
-        next.prev = prev;
-        prev.next = next;
-    }
-    
-    void updateNodeToHead(Node node) {
-        Node temp = head.next;
-        head.next = node;
-        node.prev = head;
-        node.next = temp;
-        temp.prev = node;
-    }
-}
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
- /*
+    private void intializeNode(Node head, Node tail) {
+        head.prev = null;
+        head.next = tail;
+        tail.prev = head;
+        tail.next = null;
+    }
+    /*
         ["LRUCache","put","put","get","put","get","put","get","get","get"]
         [[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
-        {H}-> <-[1,1]-> -> <-[2,2]-> <- <-{T}
      */
+    public int get(int key) {
+        Node res = LRUCacheMap.getOrDefault(key, null);
+        if(res != null){
+            int val = res.value;
+            delNode(res);
+            setNode(res);
+            return val;
+        }
+        return -1;
+    }
+
+    private void setNode(Node res) {
+        res.prev = tail.prev;
+        tail.prev.next = res;
+        res.next = tail;
+        tail.prev = res;
+    }
+
+    private void delNode(Node res) {
+        res.next.prev = res.prev;
+        res.prev.next = res.next;
+    }
+
+    public void put(int key, int value) {
+        if(LRUCacheMap.get(key) != null){
+            Node node = LRUCacheMap.get(key);
+            node.value = value;
+            delNode(node);
+            setNode(node);
+        }else{
+            Node node = new Node(value, key);
+            LRUCacheMap.put(key, node);
+            if(count < capacity){
+                ++count;
+                setNode(node);
+            }else{
+                Node dNode = head.next;
+                LRUCacheMap.remove(dNode.key);
+                delNode(dNode);
+                setNode(node);
+            }
+        }
+    }
+
+}
