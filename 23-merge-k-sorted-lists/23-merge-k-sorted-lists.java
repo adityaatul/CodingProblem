aba26f07e5c6d4b74beb13f4a29f5eb8c7678663
Atul Aditya <atul100aditya@gmail.com>
2022-01-05 21:06:18 +0530

    Time: 400 ms (5.00%), Space: 44.1 MB (20.67%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -11,26 +11,59 @@
 
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode ans = null, head = null;
-        PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> (a.val - b.val));
+//         O(Nlogk) solution with O(k) space.
+//          ListNode ans = null, head = null;
+//         //Comparator on ListNode to make PQ on node value
+//         PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> (a.val - b.val));
         
-        for (ListNode l : lists) {
-            if (l != null) {
-                queue.add(l);    
+//         for (ListNode l : lists) {
+//             if (l != null) {
+//                 queue.add(l);    
+//             }
+//         }
+        
+//         while (!queue.isEmpty()) {
+//             ListNode temp = queue.poll();
+//             if (ans == null) {
+//                 ans = temp;
+//                 head = ans;
+//             } else {
+//                 ans.next = temp;
+//                 ans = ans.next;
+//             }
+//             if (temp.next != null) {
+//                 queue.add(temp.next);    
+//             }
+//         }
+//         return head;
+        // if (lists == null || lists.length == 1 && lists[0] == null) {
+        //     return null;
+        // }
+        ListNode head = null, temp = null;
+        int size = lists.length;
+        int seen = 0;
+        
+        while (size != seen) {
+            int min = 100009, pos = -1;
+            for (int i = 0; i < size; i++) {
+                if (lists[i] != null && lists[i].val < min) {
+                    min = lists[i].val;
+                    pos = i;
+                }
             }
-        }
-        
-        while (!queue.isEmpty()) {
-            ListNode temp = queue.poll();
-            if (ans == null) {
-                ans = temp;
-                head = ans;
+            if (pos == -1) {
+                break;
+            }
+            if (temp == null) {
+                temp = lists[pos];
+                head = temp;
             } else {
-                ans.next = temp;
-                ans = ans.next;
+                temp.next = lists[pos];
+                temp = temp.next; 
             }
-            if (temp.next != null) {
-                queue.add(temp.next);    
+            lists[pos] = lists[pos].next;
+            if (lists[pos] == null) {
+                ++seen;
             }
         }
         return head;
